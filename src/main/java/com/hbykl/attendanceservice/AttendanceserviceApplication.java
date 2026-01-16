8383eff0bb781cfe1d1aee70352b60905ad99184
Hüsna BIYIKLI <108001397+hbykl@users.noreply.github.com>
2026-01-16 15:28:18 +0300

    Initial commit

---
+++ src/main/java/com/hbykl/attendanceservice/AttendanceserviceApplication.java
@@ -0,0 +1,13 @@
+package com.hbykl.attendanceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendanceserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttendanceserviceApplication.class, args);
+	}
+
+}
